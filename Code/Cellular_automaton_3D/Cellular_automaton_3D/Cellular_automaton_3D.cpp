6dc43dc67eaf415781fb30cd0cb76d89530db111
Fabian Arnold <fabi.arnold@ci4you.com>
2013-01-13 15:20:23 +0100

    Render speed dialog added

--- Code/Cellular_automaton_3D/Cellular_automaton_3D/Cellular_automaton_3D.cpp
+++ Code/Cellular_automaton_3D/Cellular_automaton_3D/Cellular_automaton_3D.cpp
@@ -3,20 +3,26 @@
 
 #include "stdafx.h"
 #include "Cellular_automaton_3D.h"
-
+#include "GL\glew.h"
+#include <gl\glu.h>
+#include <iostream>
+#include <Commctrl.h>
 #define MAX_LOADSTRING 100
 
 // Global Variables:
 HINSTANCE hInst;								// current instance
 TCHAR szTitle[MAX_LOADSTRING];					// The title bar text
 TCHAR szWindowClass[MAX_LOADSTRING];			// the main window class name
+HDC hDC;
+HGLRC hRC; 
+double m_renderSpeed = 16;
 
 // Forward declarations of functions included in this code module:
 ATOM				MyRegisterClass(HINSTANCE hInstance);
 BOOL				InitInstance(HINSTANCE, int);
 LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
-
+INT_PTR CALLBACK    ChangeTargetSpeed(HWND, UINT, WPARAM, LPARAM);
 int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
                      _In_ LPTSTR    lpCmdLine,
@@ -41,8 +47,50 @@ int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
 	}
 
 	hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_CELLULAR_AUTOMATON_3D));
+	bool bQuit = false;
+	 while (!bQuit)
+    {
+        /* check for messages */
+        if (PeekMessage (&msg, NULL, 0, 0, PM_REMOVE))
+        {
+            /* handle or dispatch messages */
+            if (msg.message == WM_QUIT)
+            {
+                bQuit = TRUE;
+            }
+            else
+            {
+                TranslateMessage (&msg);
+                DispatchMessage (&msg);
+            }
+        }
+        else
+        {
+            /* OpenGL animation code goes here */
+			glLoadIdentity();
+			gluLookAt(
+			0,0,-5,
+			0,0,0,
+			0,1,0);
+           
+            
+			glClearColor(1.0f, 0.0f, 0.0f, 0.0f) ;
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	// Main message loop:
+			glColor3f(1,1,1);
+			glBegin(GL_QUADS);
+			glVertex2f(-1,-1);
+			glVertex2f(1,-1);
+			glVertex2f(1,1);
+			glVertex2f(-1,1);
+			glEnd();
+            SwapBuffers (hDC);
+
+            Sleep ( m_renderSpeed);
+        }
+    }
+
+/*	// Main message loop:
 	while (GetMessage(&msg, NULL, 0, 0))
 	{
 		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
@@ -50,10 +98,66 @@ int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
 		}
+		else{
+			
+		}
 	}
-
+	*/
 	return (int) msg.wParam;
 }
+
+
+
+/*******************
+ * Enable OpenGL
+ *
+ *******************/
+
+void EnableOpenGL (HWND hWnd, HDC *hDC, HGLRC *hRC)
+{
+    PIXELFORMATDESCRIPTOR pfd;
+    int iFormat;
+
+    /* get the device context (DC) */
+    *hDC = GetDC (hWnd);
+
+    /* set the pixel format for the DC */
+    ZeroMemory (&pfd, sizeof (pfd));
+    pfd.nSize = sizeof (pfd);
+    pfd.nVersion = 1;
+    pfd.dwFlags = PFD_DRAW_TO_WINDOW | 
+      PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+    pfd.iPixelType = PFD_TYPE_RGBA;
+    pfd.cColorBits = 24;
+    pfd.cDepthBits = 16;
+    pfd.iLayerType = PFD_MAIN_PLANE;
+    iFormat = ChoosePixelFormat (*hDC, &pfd);
+    SetPixelFormat (*hDC, iFormat, &pfd);
+
+    /* create and enable the render context (RC) */
+    *hRC = wglCreateContext( *hDC );
+    wglMakeCurrent( *hDC, *hRC );
+	glewInit();
+	glEnable(GL_DEPTH_TEST);
+    
+
+	glClearColor(1.0, 0.0, 0.0, 0.0);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	//glOrtho(-1.0, 1.0, -1.0, 1.0, -100.0, 100.0);
+	RECT rect;
+	GetWindowRect(hWnd, &rect);
+	int width = rect.right - rect.left;
+	int height = rect.bottom - rect.top;
+	gluPerspective(45.0,width/(double)height,0.00001,100.0);
+	
+	
+	glMatrixMode(GL_MODELVIEW);
+
+
+
+}
+
 
 
 
@@ -102,11 +206,13 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
    hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
       CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
 
+   EnableOpenGL(hWnd, &hDC, &hRC);
+
    if (!hWnd)
    {
       return FALSE;
    }
-
+   
    ShowWindow(hWnd, nCmdShow);
    UpdateWindow(hWnd);
 
@@ -127,7 +233,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	int wmId, wmEvent;
 	PAINTSTRUCT ps;
-	HDC hdc;
+	//HDC hdc;
 
 	switch (message)
 	{
@@ -140,6 +246,10 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		case IDM_ABOUT:
 			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
 			break;
+		case IDM_CHANGE_TARGET_SPEED1:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_TARGETSPEED), hWnd, ChangeTargetSpeed);
+			break;
+
 		case IDM_EXIT:
 			DestroyWindow(hWnd);
 			break;
@@ -147,11 +257,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 			return DefWindowProc(hWnd, message, wParam, lParam);
 		}
 		break;
-	case WM_PAINT:
-		hdc = BeginPaint(hWnd, &ps);
-		// TODO: Add any drawing code here...
-		EndPaint(hWnd, &ps);
-		break;
+	
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		break;
@@ -159,6 +265,47 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		return DefWindowProc(hWnd, message, wParam, lParam);
 	}
 	return 0;
+}
+INT_PTR CALLBACK ChangeTargetSpeed(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		InitCommonControls();
+		
+		SendDlgItemMessage(hDlg, IDC_SLIDER1, TBM_SETRANGE, (LPARAM) (BOOL)TRUE, (LPARAM)MAKELONG(0, 1000));
+		SendDlgItemMessage(hDlg, IDC_SLIDER1, TBM_SETPOS, (LPARAM) (BOOL)TRUE, (WPARAM)m_renderSpeed);
+		char buffer2 [5];
+		_itoa_s(m_renderSpeed, buffer2, 10);
+		SetDlgItemTextA(hDlg, IDC_CURRENTSPEED, buffer2);
+		
+		return (INT_PTR)TRUE;
+	
+	case WM_COMMAND:
+		
+		if (LOWORD(wParam) == IDOK)
+		{
+			m_renderSpeed = SendDlgItemMessage(hDlg, IDC_SLIDER1, TBM_GETPOS, 0, 0);
+			//std::cout << m_renderSpeed << "\n";
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}else if(LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	case WM_HSCROLL:
+	//default:
+		int newSpeed = SendDlgItemMessage(hDlg, IDC_SLIDER1, TBM_GETPOS, 0, 0);
+		char buffer [5];
+		_itoa_s(newSpeed, buffer, 10);
+		SetDlgItemTextA(hDlg, IDC_CURRENTSPEED, buffer);
+		//SendDlgItemMessage(hDlg, IDC_CURRENTSPEED, WM_SETTEXT, 0,(LPARAM) buffer);
+		break;
+	}
+	return (INT_PTR)FALSE;
 }
 
 // Message handler for about box.
@@ -168,6 +315,7 @@ INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 	switch (message)
 	{
 	case WM_INITDIALOG:
+
 		return (INT_PTR)TRUE;
 
 	case WM_COMMAND:
@@ -180,3 +328,4 @@ INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 	}
 	return (INT_PTR)FALSE;
 }
+
